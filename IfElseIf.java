47098a71ed07a698a0fdf521b8c9e84015cfeedc
khangit2010 <71223037+khangit2010@users.noreply.github.com>
2024-07-25 10:00:22 -0400

    Add files via upload

---
+++ IfElseIf.java
@@ -0,0 +1,21 @@
+package allJavaProgram;
+
+public class IfElseIf {
+
+	public static void main(String[] args) {
+
+		int a = 5;
+		int b = 10;
+		int c = 15;
+		int d = 20;
+
+		if (a == 5 && b > 10 && c == 15) {
+
+			System.out.println("false");
+		} else if (b > a && d > c && b == 10) {
+			System.out.println("true");
+
+		}
+
+	}
+}
